9dbcea38f1c85eaea0cc000d252d1c1fd325d788
Antons Kims <antons.kims@gmail.com>
2023-02-20 23:41:45 +0100

    add layout, css and basic functionality

---
+++ index.js
@@ -0,0 +1,43 @@
+const startingSum = document.getElementById('starting-sum');
+const percent = document.getElementById('percent');
+const days = document.getElementById('days');
+const resultTable = document.getElementById('result-table');
+
+document.getElementById('submit-form').addEventListener('submit', (e) => {
+  resultTable.innerHTML = '';
+  e.preventDefault();
+  getResult();
+});
+
+const getResult = () => {
+  let totalSum = parseInt(startingSum.value);
+  let daySum = 0;
+  for (let i = 1; i <= parseInt(days.value); i++) {
+    if (document.getElementById('compound-percent').checked) {
+      daySum = (totalSum / 100) * percent.value;
+      totalSum += daySum;
+      getResultHtml(i, daySum, totalSum);
+    } else {
+      daySum = (startingSum.value / 100) * percent.value;
+      totalSum += daySum;
+      getResultHtml(i, daySum, totalSum);
+    }
+  }
+
+  startingSum.value = '';
+  percent.value = '';
+  days.value = '';
+};
+
+const getResultHtml = (day, daySum, totalSum) => {
+  document.getElementById('result-container').style.display = 'block';
+  resultTable.innerHTML += `
+        <li class="result-item">
+          <p>День: ${day}</p>
+          -
+          <p>Начислено: $${daySum.toFixed(2)}</p>
+          -
+          <p>Итоговая сумма: $${totalSum.toFixed(2)}</p>
+        </li>
+  `;
+};
